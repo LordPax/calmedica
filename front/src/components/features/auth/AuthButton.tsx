23d5e2f769fff834abb5be9fdc38dece9d2fcb66
Pietrucci-Blacher <maxime.pietrucci@gmail.com>
2024-06-27 23:30:37 +0200

    Fix: Add Dashboard to dropdown

--- front/src/components/features/auth/AuthButton.tsx
+++ front/src/components/features/auth/AuthButton.tsx
@@ -2,12 +2,13 @@ import { useSession } from '@/context/sessionProvider';
 import { LoginButton } from './LoginButton';
 import { LoggedInButton } from './LoggedInButton'; // Assurez-vous d'avoir ce composant
 
+//million-ignore
 export const AuthButton = () => {
     const { accessToken } = useSession();
 
     if (accessToken) {
         const user = sessionStorage.getItem('user');
-        return <LoggedInButton user={user} />;
+        return <LoggedInButton user={user || ''} />;
     }
 
     return <LoginButton />;
